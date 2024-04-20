a68cd9b9f0f9d9b46d7b256609308c37b0354273
Iqbal Pamula <balpamula@gmail.com>
2024-04-20 22:17:34 +0700

    chore: add prettier plugin sort tailwind class

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0);
+
+  const handelCount = () => {
+    setCount(count + 1);
+  };
 
   return (
     <>
-      <div>
+      <div className="flex w-full flex-row justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,19 +21,20 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="my-4 flex flex-col items-center justify-center gap-2">
+        <button
+          className="max-w-max rounded-md bg-zinc-900 px-4 py-2 text-base font-medium text-white shadow-md"
+          onClick={handelCount}
+        >
+          Count: {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
